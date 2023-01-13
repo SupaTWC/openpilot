45aaf68ea6b3620e97db6875dcaead2043ddc6db
supatwc <superhell2001@yahoo.com>
2023-01-13 13:32:18 -0800

    resume botton

--- selfdrive/car/chrysler/carcontroller.py
+++ selfdrive/car/chrysler/carcontroller.py
@@ -166,7 +166,7 @@ class CarController:
         decel = 4
         max_gear = 9
         #stand_still = 0
-        if accel_req == 1 and CS.button_counter % 14 == 0: 
+        if accel_req == 1 and self.frame % 14 == 0: 
           can_sends.append(create_cruise_buttons(self.packer, CS.button_counter+1, 0, CS.cruise_buttons, resume=True))
           #self.resume_pressed += 1 
           
