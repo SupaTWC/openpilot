68a950fd79fe1af07595a02818fc8424bcd39967
supatwc <superhell2001@yahoo.com>
2023-02-08 11:18:24 -0800

    resume timing

--- selfdrive/car/chrysler/carcontroller.py
+++ selfdrive/car/chrysler/carcontroller.py
@@ -266,7 +266,7 @@ class CarController:
         self.hud_count += 1
 
         #resume button control
-    if CS.button_counter % 12 == 0:
+    if CS.button_counter % 6 == 0:
       # if self.reset == 0:
       #   can_sends.append(create_cruise_buttons(self.packer, CS.button_counter+1, 0, CS.cruise_buttons, resume=False))
       #   self.reset = 1
