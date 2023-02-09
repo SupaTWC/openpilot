41d013f7b46bd0dacd58d34d39e738233fd21eae
supatwc <superhell2001@yahoo.com>
2023-02-09 14:48:43 -0800

    try no gas override
    negative torque request

--- selfdrive/car/chrysler/carcontroller.py
+++ selfdrive/car/chrysler/carcontroller.py
@@ -72,7 +72,7 @@ class CarController:
         CS.longEnabled = False
 
       if CS.longAvailable:
-        if CS.button_pressed(ButtonType.cancel) or CS.out.brakePressed:
+        if CS.button_pressed(ButtonType.cancel) or CS.out.brakePressed or (CS.longEnabled and not CS.enabled):
           CS.longEnabled = False
         elif (CS.button_pressed(ButtonType.accelCruise) or \
             CS.button_pressed(ButtonType.decelCruise) or \
@@ -162,7 +162,7 @@ class CarController:
         torque = (self.accel- max(CS.out.aEgo,0)) * torque_limits
         if CS.out.vEgo > 5: torque/2
         elif CS.out.vEgo > CC.hudControl.setSpeed * 0.95: torque/3.5
-        torque = clip(round(torque,2), -7, torque_limits) 
+        
 
         if CS.engineTorque < 0 and torque > 0:# or CS.out.vEgo < 0.2:
           torque = 15
@@ -170,6 +170,7 @@ class CarController:
         #If torque is positive, add the engine torque to the torque we calculated. This is because the engine torque is the torque the engine is producing.
         else:
           torque += CS.engineTorque
+          torque = max(round(torque,2), -10) 
 
         decel = None
         
@@ -207,26 +208,26 @@ class CarController:
           
         
 
-        else:
-          max_gear = 9
-          can_sends.append(acc_command(self.packer, self.frame / 2, 0,
-                             CS.out.cruiseState.available,
-                             CS.longEnabled,
-                             accel_req,
-                             torque,
-                             max_gear,
-                             decel_req,
-                             decel,
-                             0, 1))
-          can_sends.append(acc_command(self.packer, self.frame / 2, 2,
-                              CS.out.cruiseState.available,
-                              CS.longEnabled,
-                              accel_req,
-                              torque,
-                              max_gear,
-                              decel_req,
-                              decel,
-                              0, 1))
+        # else:
+        #   max_gear = 9
+        can_sends.append(acc_command(self.packer, self.frame / 2, 0,
+                            CS.out.cruiseState.available,
+                            CS.longEnabled,
+                            accel_req,
+                            torque,
+                            max_gear,
+                            decel_req,
+                            decel,
+                            0, 1))
+        can_sends.append(acc_command(self.packer, self.frame / 2, 2,
+                            CS.out.cruiseState.available,
+                            CS.longEnabled,
+                            accel_req,
+                            torque,
+                            max_gear,
+                            decel_req,
+                            decel,
+                            0, 1))
         if self.frame % 2 == 0:
           can_sends.append(create_acc_1_message(self.packer, 0, self.frame / 2))
           can_sends.append(create_acc_1_message(self.packer, 2, self.frame / 2))
